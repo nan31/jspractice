fa7781e80f9eaaf3fa395ac68696787e5fd9f39e
nan31 <hernanm31@gmail.com>
2022-08-12 23:13:44 -0300

    entrega final

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 class Producto{
   constructor(producto, imagen, descripcion, precio){
     this.producto = producto;
@@ -6,6 +7,8 @@ class Producto{
     this.precio = precio;
   }
 }
+
+
 
 const producto1 = new Producto("Taza", "./img/taza.png", "Taza personalizada Star Wars", 1200);
 const producto2 = new Producto("Set Desayuno", "./img/setdesayuno.png", "Set personalizado para desayuno", 1600);
@@ -23,6 +26,27 @@ productos.push(producto4)
 productos.push(producto5)
 productos.push(producto6)
 
+// DESESTRUCTURACION - Ejemplo--------------------------------------------------------//
+console.log('Ejemplo Desestrcturacion')
+
+const {producto, descripcion, precio} = producto4
+console.log(producto)
+console.log(descripcion)
+console.log(precio)
+
+console.log(producto4)
+
+
+// SPREAD - Ejemplo--------------------------------------------------------------------//
+
+console.log('Ejemplo Spread')
+
+const proximosProductos = ["Hoppie", "Billeteras", "Posavasos", "Fundas para celular"]
+console.log(...proximosProductos)
+
+console.log(proximosProductos)
+
+//-------------------------------------------------------------------------------------//
 
 function mostrarProductos(productos) {
   const contenedorDeProductos =  document.getElementById("contenedor-de-productos");
@@ -62,6 +86,7 @@ function mostrarDetalle(producto){
   `;
 
   crearBotonAgregarAlCarrito();
+  // botonToastify(productos);
 }
 
 function crearBotonVolver(){
@@ -80,13 +105,81 @@ function crearBotonAgregarAlCarrito(){
   botonComprar.classList.add("boton-comprar");
   botonComprar.innerText = "Agregar al Carrito";
   botonComprar.addEventListener("click", () => {
-    agregarAlCarrito(producto);
+    agregarAlCarrito(productos);
   }
   )
   document.getElementById("contenedor-de-productos").prepend(botonComprar);
 }
 
+function agregarAlCarrito(){
+  const carrito =  document.getElementById("carrito");
+  carrito.innerHTML = "";
+  productos.forEach(producto => {
+    let li = document.createElement("li");
+    li.innerHTML = `
+      <hr> ${producto.producto.toUpperCase()} - $ ${producto.precio}`;
+      const botonEliminar = document.createElement("button");
+      botonEliminar.innerText = "Eliminar";
+      botonEliminar.addEventListener("click", () => {
+        eliminarProducto(producto);
+      })
+      li.appendChild(botonEliminar);
+      carrito.appendChild(li);
+  });
+
+}
+
+
+// INCORPORANDO LIBRERIAS  ------------------------------------------------------------//
+
+// Sweeet Alert------------------------------//
+
+// function agregarAlCarrito(){
+//   Swal.fire({
+//     icon: 'error',
+//     title: 'Error!...',
+//     text: 'Pagina en construccion, disculpe las molestias!',
+  
+//   })
+// }
+
+//Toastify-----------------------------------//
+
+// function botonToastify(){
+//   const botonToastify = document.createElement("button");
+//   botonToastify.classList.add("boton-toastify");
+//   botonToastify.innerText = "Prueba Toastify";
+//   botonToastify.addEventListener("click", () => {
+//     Toastify({
+//       text: "Prueba Toastify correcta!!!",
+//       duration: 3000 
+//       }).showToast();
+  
+// })
+// document.getElementById("contenedor-de-productos").prepend(botonToastify);
+// }
+
+
+// // Incorporando Fetch a JSON ----------------------------------------------------------//
+
+// console.log('Incorporando Fetch')
+
+// const URL = '/elementos.json'
+
+//   function renderElementos (elementos){
+//     elementos.forEach( elemento =>{
+//       document.write(`<li>${elemento.nombre} - precio: $ ${elemento.precio}</li>`)
+//     });
+//   }
+
+// fetch (URL)
+
+//     .then(res => res.json() )
+//     .then(datos => {renderElementos(datos)})
+//     .finally( ()=> {console.log('Termino el Fetch')})
+
+// //---------------------------------------------------------------------------------------//
+
+//ver entrega final
 
 mostrarProductos(productos);
-
-
